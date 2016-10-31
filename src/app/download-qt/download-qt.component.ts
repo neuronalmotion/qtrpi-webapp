7672a5f780c7ac974a1c2e2c7e7b79b75eeb8cba
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-31 11:56:26 +0100

    Clean download page

--- src/app/download-qt/download-qt.component.ts
+++ src/app/download-qt/download-qt.component.ts
@@ -9,56 +9,10 @@ export class DownloadQtComponent implements OnInit {
 
     qtVersions: QtVersion[] = [
         {
-            versionName: '5.7.1',
+            versionName: '5.7.0',
             binaries: [
                 {
-                    module: 'QtCore',
-                    description: 'QtBase, etc...',
-                    urlRPi1: '/download/1',
-                    urlRPi2: '/download/2',
-                    urlRPi3: '/download/3'
-                },
-                {
-                    module: 'Qt3D',
-                    description: 'qt3d....',
-                    urlRPi1: '/download/1',
-                    urlRPi2: '/download/2',
-                    urlRPi3: '/download/3'
-                }
-            ]
-        },
-        {
-            versionName: '5.6.2',
-            binaries: [
-                {
-                    module: 'QtCore',
-                    description: 'QtBase, etc...',
-                    urlRPi1: '/download/1',
-                    urlRPi2: '/download/2',
-                    urlRPi3: '/download/3'
-                },
-                {
-                    module: 'Qt3D',
-                    description: 'qt3d....',
-                    urlRPi1: '/download/1',
-                    urlRPi2: '/download/2',
-                    urlRPi3: '/download/3'
-                }
-            ]
-        },
-        {
-            versionName: '5.5.1',
-            binaries: [
-                {
-                    module: 'QtCore',
-                    description: 'QtBase, etc...',
-                    urlRPi1: '/download/1',
-                    urlRPi2: '/download/2',
-                    urlRPi3: '/download/3'
-                },
-                {
-                    module: 'Qt3D',
-                    description: 'qt3d....',
+                    modules: 'Qt Base, Qt Declarative (Quick 2), Qt Quick Controls, Qt Quick Controls 2, Qt 3D',
                     urlRPi1: '/download/1',
                     urlRPi2: '/download/2',
                     urlRPi3: '/download/3'
