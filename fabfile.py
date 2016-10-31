8cbba0f5b4adae6ff979fa95bed59d45c6c634e7
Robin Penea <robin.penea@gmail.com>
2016-10-31 12:05:33 +0100

    Upload binaries with Ansible
    Close #7

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,12 @@
 from fabric.api import local, warn_only
 
 def prepare_dev_environment():
-    local('ansible-playbook ansible/dev-environment.yml')
-    ansible_install()
-
-def ansible_install():
-    with warn_only():
-        local('sudo mkdir /etc/ansible')
     local('sudo ansible-galaxy install -r ansible/requirements.yml')
+    local('ansible-playbook ansible/dev-environment.yml')
 
 def deploy():
-    local('ansible-playbook ansible/site.yml')
+    local('ansible-playbook ansible/site.yml --tags webapp')
+
+def upload_dist():
+    local('ansible-playbook ansible/site.yml --tags binary')
 
