e18e0bc803ed19e31ab746efce0648a0d273ace6
Robin Penea <robin.penea@gmail.com>
2016-11-02 18:55:08 +0100

    Update download link for qt 5.7.0 binary

--- src/app/download-qt/download-qt.component.ts
+++ src/app/download-qt/download-qt.component.ts
@@ -13,9 +13,9 @@ export class DownloadQtComponent implements OnInit {
             binaries: [
                 {
                     modules: 'Qt Base, Qt Declarative (Quick 2), Qt Quick Controls, Qt Quick Controls 2, Qt 3D',
-                    urlRPi1: '/downloads/qt/rpi1/none',
-                    urlRPi2: '/downloads/qt/rpi2/qtrpi-linux-rasp-pi2-g%2B%2B_qt-5.7_20161031.zip',
-                    urlRPi3: '/downloads/qt/rpi3/none',
+                    urlRPi1: '/downloads/qtrpi/rpi1/none',
+                    urlRPi2: '/downloads/qtrpi/rpi2/qtrpi-rpi2_qt-5.7.0.zip',
+                    urlRPi3: '/downloads/qtrpi/rpi3/none',
                     releaseDate: '2016-11-02'
                 }
             ]
